ea3d1f2c857a65b79be31d51097c4ff159704675
Maria <fmariamarinescu@gmail.com>
2019-12-15 14:02:12 +0200

    tries to do things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import NavBar from "./components/navbar/Navbar.component";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/'
 
-function App() {
+// New - import the React Router components, and the Profile page component
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/profile/Profile.component";
+import history from "./utils/history";
+import PrivateRoute from "./components/PrivateRoute";
+import Welcome from './components/Welcome.component';
+import Home from './components/Card.component';
+import Card from './components/Card.component';
+
+
+
+
+const  App = ()  => {
+
   return (
     <div className="App">
+      <Router history={history}>
+      <Welcome/>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact/>
+          <Route path="/profile" component={Profile} />
+          <PrivateRoute path="/profile" component={Profile} />
+          <Route path="/home" component={Home} />
     
+        </Switch>
+
+      </Router>
     </div>
   );
 }
