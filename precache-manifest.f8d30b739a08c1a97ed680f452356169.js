96f4326e2bfad06164e61aee1e3a7df799ab6dbe
rriamarria <fmariamarinescu@gmail.com>
2020-01-19 15:49:57 +0200

    Updates

---
+++ precache-manifest.f8d30b739a08c1a97ed680f452356169.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6410e577e3339c2aaf65d552ae43f5a6",
+    "url": "/FarAway_Matching/./index.html"
+  },
+  {
+    "revision": "3d897de31f8bff3d7bd5",
+    "url": "/FarAway_Matching/./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "22a82da554a44037d972",
+    "url": "/FarAway_Matching/./static/css/main.7cf94d23.chunk.css"
+  },
+  {
+    "revision": "3d897de31f8bff3d7bd5",
+    "url": "/FarAway_Matching/./static/js/2.bbc69c8a.chunk.js"
+  },
+  {
+    "revision": "22a82da554a44037d972",
+    "url": "/FarAway_Matching/./static/js/main.14d0acde.chunk.js"
+  },
+  {
+    "revision": "917ebc96b000c4c68ff2",
+    "url": "/FarAway_Matching/./static/js/runtime-main.08628cd5.js"
+  },
+  {
+    "revision": "ade91cb0bb7c69696a7df18125ae82cb",
+    "url": "/FarAway_Matching/./static/media/DeathStar-VmWB.ade91cb0.ttf"
+  }
+]);
